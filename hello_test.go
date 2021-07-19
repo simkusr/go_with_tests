7531e27df9968f05c80ea4ec2ebad6c55f3675a8
Rolandas Simkus <rolandas.simkus@revelsystems.com>
2021-07-19 12:31:25 +0300

    refactored tests and now if empty string passed, by default we return `Hello, World`

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Rolandas")
-	want := "Hello, Rolandas"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Rolandas")
+		want := "Hello, Rolandas"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say `Hello, world` when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
