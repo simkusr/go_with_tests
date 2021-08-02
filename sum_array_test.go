c0419b638b2270c89caeaf8a8cc4a5985a7e584a
Rolandas Simkus <rolandas.simkus@revelsystems.com>
2021-08-02 08:13:39 +0300

    fix bug, converted from array to slice as slice is expected in SumArray

--- sum_array_test.go
+++ sum_array_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestSumArray(t *testing.T) {
 	t.Run("Simple test", func(t *testing.T) {
-		data := [5]int{1, 2, 3, 4, 5}
-		got := SumArray(data[:])
+		data := []int{1, 2, 3, 4, 5}
+		got := SumArray(data)
 		expected := 15
 		if got != expected {
 			t.Errorf("Expected %d but got %d while sent %v", expected, got, data)
@@ -13,8 +13,8 @@ func TestSumArray(t *testing.T) {
 	})
 
 	t.Run("Harder test", func(t *testing.T) {
-		data := [6]int{1, 2, 3, 4, 5, 6}
-		got := SumArray(data[:])
+		data := []int{1, 2, 3, 4, 5, 6}
+		got := SumArray(data)
 		expected := 21
 		if got != expected {
 			t.Errorf("Expected %d but got %d while sent %v", expected, got, data)
